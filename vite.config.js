d2525116c27433d027df4959384516537eee3df2
Muhammad Israr Khan <israruetp@gmail.com>
2024-09-26 23:24:14 +0500

    improve performance by optimizing the code

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,26 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
+import {visualizer} from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    visualizer({ open: true }),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
       },
-      manifest:{
+      manifest: {
         name: 'Music App',
-        theme_color : "#ff5e3a",
-        icons:[
-          {src:"assets/img/pwa-192x192.png",sizes: '192x192',type: "image/png"}
-        ]
+        theme_color: '#ff5e3a',
+        icons: [{ src: 'assets/img/pwa-192x192.png', sizes: '192x192', type: 'image/png' }]
       },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,png,jpg,svg}"]
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg}']
       }
       // strategies: {}
     })
